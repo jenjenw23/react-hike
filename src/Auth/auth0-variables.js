9ea66f34a73ea121939f60cd06ff801fe0ca9a98
Tom LaChance <guitarlanguageseries@gmail.com>
2018-04-28 17:35:24 -0500

    early attempt at auth0 athentication.  Currently the page immediately asks the user to login, it then pushes back to homepage and asks for login again.  Very annoying, do not merge at this time

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'guitarlanguage.auth0.com',
+  clientId: '71g1vewPfq749f1Eupt6NhiR2I5waROg',
+  callbackUrl: 'http://localhost:3000/callback'
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import App from '../src/App';
+import Front from '../src/Front';
 
 render((
   <BrowserRouter>
     <div> 
-        <Route exact path="/"  /> 
+        <Route exact path="/" component={Front}  /> 
         <Route exact path="/trails" component={App} /> 
     </div>
   </BrowserRouter>
